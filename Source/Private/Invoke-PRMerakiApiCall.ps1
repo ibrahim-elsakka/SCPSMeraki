93e26b18f81c2076ba3470cf760677d4e636ac17
ScriptingChris <christian@scriptingchris.tech>
2021-05-07 20:43:17 +0200

    push all the code

---
+++ Source/Private/Invoke-PRMerakiApiCall.ps1
@@ -0,0 +1,46 @@
+function Invoke-PRMerakiApiCall {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("POST", "GET", "PUT", "DELETE")]
+        [String]$Method,
+        [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+        [String]$Resource,
+        [Parameter(Mandatory=$true)]
+        [String]$ApiKey    
+    )
+
+    begin {
+        Write-Verbose -Message "Setting the API Key $($ApiKey) as a Script Varaible"
+        Set-Variable -Name $ApiKey -Value $apiKey -Scope Script
+        
+        $baseUrl = "https://api.meraki.com/api/v1/"
+        Write-Verbose -Message "Setting the base url: "
+
+        $headers = @{
+            "Content-Type" = "application/json"
+            "Accept" = "application/json"
+            "X-Cisco-Meraki-API-Key" = $ApiKey
+        }
+        Write-Verbose -Message "Setting the API Call headers"
+    }
+
+    process {
+        Write-Verbose -Message "Invoking the API call with uri: $($baseUrl)/$($Resource) and the Method: $($Method)"
+        try {
+            $result = Invoke-RestMethod -Uri $baseUrl/$Resource -Method $Method -Headers $headers
+            return $result
+        }
+        catch {
+            $statusCode = $_.Exception.Response.StatusCode.value__
+            $statusDescription = $_.Exception.Response.StatusDescription
+        }
+    }
+
+    end {
+        if(!($result)){
+            Write-Error -Message " HTTP Status Code: $($statusCode) - Error Description: $statusDescription"
+        }
+    }
+}
---
+++ Source/Public/Set-SCMrkAuth.ps1
@@ -0,0 +1,53 @@
+function Set-SCMrkAuth {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Please provide and API Key:")]
+        [String]$ApiKey,
+        [Parameter(Mandatory=$false)]
+        [String]$OrgId
+    )
+
+    Begin {
+        Write-Verbose -Message "Initiating Meraki Dashboard Authentication"
+    }
+
+    Process {
+        if(!($OrgId)){
+            Write-Verbose -Message "Authenticating to Meraki Dashboard API"
+            try {
+                $OrgId = Invoke-PRMerakiApiCall -Method GET -Resource "/organizations" -ApiKey $ApiKey | Select-Object -ExpandProperty Id -First 1
+                Write-Verbose -Message "Setting the OrgId Variable as Script Scope"
+                Set-Variable -Name OrgId -Value $OrgId -Scope Script
+            }
+            catch {
+                $statusCode = $_.Exception.Response.StatusCode.value__
+                $statusDescription = $_.Exception.Response.StatusDescription
+            }
+        }
+        elseif($OrgId){
+            Write-Verbose -Message "Validating Organisation Id provided in parameter agains Meraki Dashboard API"
+            try {
+                $OrgIds = Invoke-PRMerakiApiCall -Method GET -Resource "/organizations" -ApiKey $ApiKey | Select-Object -ExpandProperty Id
+                if($OrgIds -contains $OrgId){
+                    Write-Verbose -Message "Setting the OrgId Variable as Script Scope"
+                    Set-Variable -Name OrgId -Value $OrgId.ToString() -Scope Script
+                }
+                else {
+                    $statusCode = $_.Exception.Response.StatusCode.value__
+                    $statusDescription = "Organisation Id provided in Parameter, was not found in your Meraki Dashboard"
+                }
+            }
+            catch {
+                $statusCode = $_.Exception.Response.StatusCode.value__
+                $statusDescription = $_.Exception.Response.StatusDescription
+            }
+        }
+    }
+
+    End {
+        if($statusCode){
+            Write-Error -Message "Status code: $($statusCode), Error Description: $($statusDescription)"
+        }
+    }
+}
